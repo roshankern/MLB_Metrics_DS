2c6c64751103361d696d9ef9304518b57d6fe785
Roshan Kern <roshan.kern@ucdenver.edu>
2023-12-19 23:32:06 -0500

    update helper functions

--- mlb_metrics_helpers.py
+++ mlb_metrics_helpers.py
@@ -5,8 +5,17 @@ import pybaseball as pb
 import statsapi
 import pandas as pd
 
-
 from sklearn.model_selection import train_test_split
+from sklearn.compose import make_column_selector as selector
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import (
+    RandomForestClassifier,
+    GradientBoostingClassifier,
+    HistGradientBoostingClassifier,
+)
+from sklearn.pipeline import make_pipeline
 
 
 def player_id(last_name: str, first_name: str, player_num: int = 0) -> int:
@@ -132,17 +141,17 @@ def batter_model_metrics(player_specific_metrics: pd.DataFrame) -> pd.DataFrame:
         "description",
     ]
 
-    # Select only the relevant columns
-    model_data = player_specific_metrics[col_to_keep]
-    model_data = model_data[
-        (model_data["description"] == "hit_into_play")
-        | (model_data["description"] == "swinging_strike")
+    # Select only the relevant target columns
+    batter_model_data = player_specific_metrics[col_to_keep]
+    batter_model_data = batter_model_data[
+        (batter_model_data["description"] == "hit_into_play")
+        | (batter_model_data["description"] == "swinging_strike")
     ]
 
     # Drop rows with missing values
-    model_data.dropna(inplace=True)
+    batter_model_data.dropna(inplace=True)
 
-    return model_data
+    return batter_model_data
 
 
 def batter_model_datasets(
@@ -170,7 +179,94 @@ def batter_model_datasets(
 
     # Split into training and testing datasets
     X_train, X_test, y_train, y_test = train_test_split(
-        X, y, random_state=0, test_size=0.1, stratify=target
+        X, y, random_state=0, test_size=0.1, stratify=y
     )
 
     return (X_train, X_test, y_train, y_test)
+
+
+def trained_batter_model(
+    X_train: pd.DataFrame, y_train: pd.Series, sklearn_model: object
+) -> object:
+    """
+    Trains a batter model using the provided training data.
+
+    Parameters:
+        X_train (pd.DataFrame): Training feature dataset.
+        y_train (pd.Series): Training class dataset.
+        sklearn_model (object): Type of model to be trained.
+
+    Returns:
+        object
+    """
+    # Select numerical and categorical columns
+    numerical_columns_selector = selector(dtype_exclude=object)
+    categorical_columns_selector = selector(dtype_include=object)
+
+    numerical_columns = numerical_columns_selector(X_train)
+    categorical_columns = categorical_columns_selector(X_train)
+
+    # Preprocess categorical and numerical columns
+    categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")
+    numerical_preprocessor = StandardScaler()
+
+    preprocessor = ColumnTransformer(
+        [
+            ("one-hot-encoder", categorical_preprocessor, categorical_columns),
+            ("standard_scaler", numerical_preprocessor, numerical_columns),
+        ]
+    )
+
+    # Create and train the model
+    model = make_pipeline(preprocessor, sklearn_model)
+    model.fit(X_train, y_train)
+
+    return model
+
+
+def tested_batter_model(
+    batter_model_data: pd.DataFrame,
+    sklearn_model_type: Literal[
+        "logistic_regression",
+        "random_forest",
+        "gradient_boosting",
+        "hist_gradient_boosting",
+    ],
+) -> tuple[object, float]:
+    """
+    Trains and evaluates a batter model using the specified sklearn model type.
+
+    Args:
+        batter_model_data (pd.DataFrame): The input data for training the model.
+        sklearn_model_type (Literal): The type of sklearn model to use. Must be one of:
+            - "logistic_regression"
+            - "random_forest"
+            - "gradient_boosting"
+            - "hist_gradient_boosting"
+
+    Returns:
+        tuple[object, float]: A tuple containing the trained model object and the accuracy score.
+    """
+    # Split into training and testing datasets
+    X_train, X_test, y_train, y_test = batter_model_datasets(batter_model_data)
+
+    # Train the model
+    if sklearn_model_type == "logistic_regression":
+        model_type = LogisticRegression(random_state=0)
+    elif sklearn_model_type == "random_forest":
+        model_type = RandomForestClassifier(random_state=0)
+    elif sklearn_model_type == "gradient_boosting":
+        model_type = GradientBoostingClassifier(random_state=0)
+    elif sklearn_model_type == "hist_gradient_boosting":
+        model_type = HistGradientBoostingClassifier(random_state=0)
+    else:
+        raise ValueError(
+            "Invalid sklearn_model_type. Please choose a valid model type."
+        )
+
+    model = trained_batter_model(X_train, y_train, model_type)
+
+    # Evaluate the model
+    accuracy = model.score(X_test, y_test)
+
+    return (model, accuracy)
