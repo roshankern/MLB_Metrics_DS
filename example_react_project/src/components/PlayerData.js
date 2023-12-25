719b5b2284b5e3d8e47d4371ff17319276b2a793
Roshan Kern <roshan.kern@ucdenver.edu>
2023-12-24 17:59:31 -0700

    finish player data base

--- example_react_project/src/components/PlayerData.js
+++ example_react_project/src/components/PlayerData.js
@@ -1,6 +1,38 @@
 // PlayerData.js
 import React from 'react';
 import { Grid, Paper, Typography } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+
+function createMetricsTable(metricsJson) {
+    const firstTenRows = metricsJson.slice(0, 5);
+    const totalRows = metricsJson.length;
+
+    return (
+        <div>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                        <TableRow>
+                            {Object.keys(firstTenRows[0]).map((key) => (
+                                <TableCell key={key} align="center">{key}</TableCell>
+                            ))}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {firstTenRows.map((row, index) => (
+                            <TableRow key={index}>
+                                {Object.values(row).map((value, cellIndex) => (
+                                    <TableCell key={cellIndex} align="center">{value}</TableCell>
+                                ))}
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
+            <p>Total Rows: {totalRows}</p>
+        </div>
+    );
+}
 
 function formattedCareerTimeline(careerTimeline) {
     return (
@@ -16,7 +48,7 @@ function parseGeneralMetrics(generalMetrics, metricType) {
 
     let metricsInfo = fullName + '\n\n';
 
-    const metricStats = stats.find(stat => stat.group === metricType);
+    const metricStats = stats.find(stat => stat.group === (metricType === "pitching" ? "pitching" : "hitting"));
 
     if (metricStats) {
         metricsInfo += `Career ${metricType.charAt(0).toUpperCase() + metricType.slice(1)}\n`;
@@ -41,33 +73,38 @@ const PlayerData = ({ searching, data }) => {
 
     if (data) {
         return (
-            <Grid container spacing={2}>
-                <Grid item xs={12} md={6}>
-                    <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
-                        <Typography variant="h6">Player ID</Typography>
-                        <Typography>{data.playerId}</Typography>
-                    </Paper>
-                    <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
-                        <Typography variant="h6">Career Timeline</Typography>
-                        <Typography>{formattedCareerTimeline(data.careerTimeline)}</Typography>
-                    </Paper>
-                    <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
-                        <Typography variant="h6">General Metrics</Typography>
-                        <pre>{parseGeneralMetrics(data.generalMetrics, "pitching")}</pre>
-                    </Paper>
-                    <Paper elevation={3} style={{ padding: '16px' }}>
-                        <Typography variant="h6">Specific Metrics</Typography>
-                        <Typography>{JSON.stringify(data.specificMetrics, null, 2)}</Typography>
-                    </Paper>
+            <div>
+                <Grid container spacing={2}>
+                    <Grid item xs={12} md={4}>
+                        <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
+                            <Typography variant="h6">Player ID</Typography>
+                            <Typography>{data.playerId}</Typography>
+                        </Paper>
+                        <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
+                            <Typography variant="h6">Career Timeline</Typography>
+                            <Typography>{formattedCareerTimeline(data.careerTimeline)}</Typography>
+                        </Paper>
+                        <Paper elevation={3} style={{ padding: '16px', marginBottom: '16px' }}>
+                            <Typography variant="h6">General Metrics</Typography>
+                            <pre>{parseGeneralMetrics(data.generalMetrics, data.metricType)}</pre>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Paper elevation={3} style={{ padding: '16px' }}>
+                            <Typography variant="h6">Scatter Plot Box</Typography>
+                            {/* Placeholder for scatter plot */}
+                            <div>Scatter Plot Goes Here</div>
+                        </Paper>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={6}>
-                    <Paper elevation={3} style={{ padding: '16px' }}>
-                        <Typography variant="h6">Scatter Plot Box</Typography>
-                        {/* Placeholder for scatter plot */}
-                        <div>Scatter Plot Goes Here</div>
-                    </Paper>
-                </Grid>
-            </Grid>
+                <Paper elevation={3} style={{ padding: '16px' }}>
+                    <Typography variant="h6">Specific Metrics</Typography>
+                    <div>
+                        {createMetricsTable(data.specificMetrics)}
+                    </div>
+                </Paper>
+            </div>
+
         );
     }
 
